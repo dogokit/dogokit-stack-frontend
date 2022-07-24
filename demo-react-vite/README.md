5f3173cadfbf771c340538586e28006613a2acee
M Haidar Hanif <git@mhaidarhanif.com>
2022-07-25 02:29:07 +0700

    feat: demo react vite

---
+++ demo-react-vite/README.md
@@ -0,0 +1,19 @@
+# Demo React Vite
+
+## Installation
+
+```sh
+npm install
+```
+
+## Development
+
+```sh
+npm run dev
+```
+
+## Build
+
+```sh
+npm run build
+```
---
+++ demo-react-vite/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 className="text-teal-500 text-5xl font-bold underline">
+          Hello world!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
